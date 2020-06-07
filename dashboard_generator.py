6cad350eecc6f7d742d18f70591024eb951d7f41
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-07 07:44:51 -0500

    add csv files

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,5 +1,22 @@
 # dashboard_generator.py
 
+import csv
+import os 
+
+def to_usd(my_price):
+  # return "${0:,.2f}".format(my_price)
+  return f"${my_price:,.2f}"
+
+# INFO INPUTS
+
+csv_filename = "sales-201803.csv" 
+
+csv_filepath = os.path.join("data", csv_filename)
+
+rows = [] 
+
+
+
 print("-----------------------")
 print("MONTH: March 2018")
 
